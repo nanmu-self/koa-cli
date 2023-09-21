4c67cf6c5f64f5b2d6c1907e3e54f4d76ed77d8b
楠木 <157884200@qq.com>
2023-09-21 20:30:55 +0800

    添加静态资源返回

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const Koa = require("koa");
+const path = require("path");
+const static = require("koa-static");
 const InitManager = require("./core/init");
 require("dotenv").config();
 // 获取参数
@@ -8,6 +10,7 @@ const catchError = require("./middlewares/exception");
 const app = new Koa();
 app.use(catchError);
 app.use(parser());
+app.use(static(path.join(__dirname, "./static")));
 
 InitManager.initCore(app);
-app.listen(3000);
+app.listen(process.env.Port);
